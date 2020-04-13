1ca44d8e9c88b755f5c3806172250cbd5b4c0c74
Khoi Le <khoibaka@gmail.com>
2020-04-13 19:26:04 -0400

    Add messaging room

---
+++ gameroom-front/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import MainMenu from './MainMenu/JS/MainMenu'
+import './App.css';
+import Notification from './MainMenu/JS/Notification';
+import Rating from './Lobby/JS/Rating';
+import Lobby from './Lobby/JS/Lobby';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.user = {
+      profileIcon: "https://i.ibb.co/sg0q559/Featherknight-Summoner-Icon-TFT-Lo-L.jpg",
+      name: 'Scarria1',
+      rating: 4.3
+    }
+    this.toNoti = this.toNoti.bind(this);
+    this.toRating = this.toRating.bind(this);
+    this.toMainMenu = this.toMainMenu.bind(this);
+    this.state = {component: <MainMenu user={this.user} toNoti={this.toNoti}></MainMenu>};
+  }
+
+  toNoti(players){
+    // console.log('to noti');
+    this.setState({component: <Notification players={players} toRating={this.toRating}></Notification>});
+    console.log(this.state);
+  }
+
+  toRating(players){
+    this.setState({component: <Rating players={players} toMainMenu={this.toMainMenu}></Rating>});
+  }
+
+  toMainMenu(){
+    this.setState({component: <MainMenu user={this.user} toNoti={this.toNoti}></MainMenu>});
+  }
+  render(){
+    return (
+      <div className="App">
+        <Lobby></Lobby>
+        {/* {this.state.component} */}
+        {/* <Rating players= {[{name: 'Scarria1'},{name: 'Scarria2'},{name: 'Scarria3'},{name: 'Scarria4'},{name: 'Scarria5'}]}></Rating> */}
+        {/* <Notification 
+        players= {[{name: 'Scarria1'},{name: 'Scarria2'},{name: 'Scarria3'},{name: 'Scarria4'},{name: 'Scarria5'},{name: 'Scarria6'}]}
+        ></Notification> */}
+      </div>
+    );
+    
+  }
+} 
+export default App;
---
+++ gameroom-front/Lobby/JS/Message.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../Style/Lobby.css';
+function Message(props) {
+    let message = props.message;
+    return (
+        <li key={message.id}>
+            <div className='message'>
+            <div className='message-username'>
+                {message.username}
+            </div>
+            <div className='bounding'>
+                <div className='message-text'>
+                    {message.text}
+                </div>
+            </div>
+            
+        </div>
+            
+        </li>
+    )
+}
+
+export default Message;
